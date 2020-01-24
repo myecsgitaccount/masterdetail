38e295a46cdb4f6f465be7f10d08f6c29d8c71f1
onyekachila <onyekachila@gmail.com>
2020-01-24 19:36:07 +0100

    complete model configuration using entity framework code first and fluent api

---
+++ DataLayer/WorkOrderConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using MasterDetail.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace MasterDetail.DataLayer
+{
+    public class WorkOrderConfiguration : EntityTypeConfiguration<WorkOrder>
+    {
+        public WorkOrderConfiguration()
+        {
+            Property(wo => wo.OrderDateTime).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Computed);
+            Property(wo => wo.Description).HasMaxLength(256).IsOptional();
+            Property(wo => wo.Total).HasPrecision(18, 2).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Computed); 
+            Property(wo => wo.CertificationRequirements).HasMaxLength(120).IsOptional();
+        }
+    }
+}
--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using MasterDetail.DataLayer;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
@@ -24,6 +25,28 @@ namespace MasterDetail.Models
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<InventoryItem> InventoryItems { get; set; }
+        public DbSet<Labor> Labors { get; set; }
+        public DbSet<Part> Parts { get; set; }
+        public DbSet<ServiceItem> ServiceItems { get; set; }
+        public DbSet<WorkOrder> WorkOrders { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new CategoryConfiguration());
+            modelBuilder.Configurations.Add(new CustomerConfiguration());
+            modelBuilder.Configurations.Add(new InventoryItemConfiguration());
+            modelBuilder.Configurations.Add(new LaborConfiguration());
+            modelBuilder.Configurations.Add(new PartConfiguration());
+            modelBuilder.Configurations.Add(new ServiceItemConfiguration());
+            modelBuilder.Configurations.Add(new WorkOrderConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
 
         public static ApplicationDbContext Create()
         {
